6e6407ff8c0d987908646360cee2b0f250d89a3e
vdj4y <alfredovidy@live.com>
2016-10-21 23:54:08 +0700

    type: Refactor
    - add recipe handler functions to ./handlers/recipes.js
    - routes now will use functions from ./handlers/recipes.js

--- server.js
+++ server.js
@@ -4,35 +4,8 @@ const db = new Sqlite3.Database("./dindin.sqlite");
 const server = new Hapi.Server();
 server.connection({port: 4000});
 
-server.route([{
-	method: "GET",
-	path: "/api/recipes",
-	handler(request, reply){
-		let sql = "SELECT * FROM recipes";
-		const params = [];
-		console.log(request.query)
-		if(request.query.cuisine){
-			sql += "WHERE cuisine = ?";
-			params.push(request.query.cuisine);
-		}
-		db.all(sql, params, (err, results) => {
-			if(err) throw err;
-			reply(results);
-		})
-	}
-},{
-	method: "GET",
-	path: "/api/recipes/{id}",
-	handler(request, reply){
-		db.get("SELECT * FROM recipes WHERE id = ?", [request.params.id], (err , result) => {
-			if(err) throw err;
-			if(typeof result !== "undefined"){
-				return reply(result)
-			}
-			return reply("Not found").code(404);
-		})
-	}
-}]);
+server.bind({db: db});
+server.route(require("./routes"));
 
 server.start((err) => {
 	if(err) throw err;
